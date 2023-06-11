46225cbc8e938730d01d3062dd74e3a82120a3ea
CPanozo23 <cpanozo@outlook.com>
2023-06-10 22:45:18 -0400

    Add Readme

---
+++ readme.md
@@ -0,0 +1,17 @@
+# EJEMPLO REACT VITE: PokéApi
+## Temas
+* [1. Introducción](#1-Introducción)
+* [2. Características](#2-Características)
+* [3. WebSite](#3-WebSite)
+
+## 1. Introducción
+
+Este es un ejemplo de React como parte de Bootcamp Full Stack con JavaScript.
+
+## 2. Características
+
+El ejemplo incluye Vite, conexión a PokéApi y Bootstrap.
+
+## 3. WebSite
+ 
+[Sitio Web: https://react-pokeapi-cpanozo.netlify.app/](https://react-pokeapi-cpanozo.netlify.app/)
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,7 @@ export const NavBar = () => {
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
             <div className="container-fluid">
-                <NavLink className="nav-item nav-link text-white fs-4 mx-5" to="/">API Pokemón</NavLink>
+                <NavLink className="nav-item nav-link text-white fs-4 mx-5" to="/">API Pokémon</NavLink>
                 
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
